b95cbf77d9fe293e9c8b463cbc3f00d9641fa2a6
Mechlus <82886093+darttheg@users.noreply.github.com>
2026-01-15 14:05:37 -0500

    Cleanup

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-cd /d .\LimeBuilder\x64\Release
-LimeBuilder ..\..\..\LimeEngine\x64\Release
-cd /d ..\..\..\
+pushd "%~dp0LimeBuilder\x64\Release" || exit \b 1
+LimeBuilder.exe "%~dp0LimeEngine/x64/Release"
+popd
